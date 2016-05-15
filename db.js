efd0a2696371906af8a51951160fda7810403aa7
Ben Eisenberg <nebster100@berkeley.edu>
2016-05-15 12:31:30 -0400

    Add postgres for production

--- db.js
+++ db.js
@@ -3,12 +3,21 @@ all of the stuff to sequelize the data. It gets
 called in server.js which in turn calls the instance
 of sequelize in this file.
 */
-
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev_toDo_API.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if(env === 'production'){
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
+}
+else{}
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev_toDo_API.sqlite'
+	});
+}
 var db = {};
 
 db.toDo = sequelize.import(__dirname + '/models/toDo.js');
